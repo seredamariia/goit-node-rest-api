dca4158e828de77c5eebd235f983c3c72a004929
Mariia Sereda <seredamariiav@gmail.com>
2024-06-12 05:32:48 +0300

    Performed Step 3.

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,73 @@
-import contactsService from "../services/contactsServices.js";
+const contactsService = require("../services/contactsServices");
+const { HttpError } = require("../helpers/HttpError");
 
-export const getAllContacts = (req, res) => {};
+const getAllContacts = async (req, res, next) => {
+  try {
+    const result = await contactsService.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getOneContact = (req, res) => {};
+const getOneContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.getContactById(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const deleteContact = (req, res) => {};
+const deleteContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.removeContact(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const createContact = (req, res) => {};
+const createContact = async (req, res, next) => {
+  try {
+    const result = await contactsService.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const updateContact = (req, res) => {};
+const updateContact = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contactsService.updateContact(id, req.body);
+
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+      throw HttpError(400, "Body must have at least one field");
+    }
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = {
+  getAllContacts,
+  getOneContact,
+  deleteContact,
+  createContact,
+  updateContact,
+};
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -1,11 +1,19 @@
-import express from "express";
-import {
+const express = require("express");
+
+const {
   getAllContacts,
   getOneContact,
   deleteContact,
   createContact,
   updateContact,
-} from "../controllers/contactsControllers.js";
+} = require("../controllers/contactsControllers");
+
+const { validateBody } = require("../helpers/validateBody");
+
+const {
+  createContactSchema,
+  updateContactSchema,
+} = require("../schemas/contactsSchemas");
 
 const contactsRouter = express.Router();
 
@@ -15,8 +23,8 @@ contactsRouter.get("/:id", getOneContact);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post("/", validateBody(createContactSchema), createContact);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
 
-export default contactsRouter;
+module.exports = contactsRouter;
